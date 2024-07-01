1e43548491f993f28ba41506d726ea6fd9b1660d
vuthanhlong1211 <21020647@vnu.edu.vn>
2024-07-01 21:01:26 +0700

    add write and read latest scheme config in json

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import customtkinter as CTk
 from tkinter import filedialog
 import json
@@ -178,12 +179,15 @@ class App(CTk.CTk):
 
         self.rest_duration_label = CTk.CTkLabel(self.recording_duration_config_frame, text="Thời gian nghỉ (giây):", wraplength=200,font=("Arial", 18))
         self.rest_duration_entry = CTk.CTkEntry(self.recording_duration_config_frame, placeholder_text="2", height=40, font=("Arial", 18))
+        self.rest_duration = 0
         
         self.cue_duration_label = CTk.CTkLabel(self.recording_duration_config_frame, text="Thời gian gợi ý (giây):", wraplength=200, font=("Arial", 18))
         self.cue_duration_entry = CTk.CTkEntry(self.recording_duration_config_frame, placeholder_text="2", height=40, font=("Arial", 18))
+        self.cue_duration = 0
         
         self.action_duration_label = CTk.CTkLabel(self.recording_duration_config_frame, text="Thời gian thực hiện (giây):", wraplength=250, font=("Arial", 18))
         self.action_duration_entry = CTk.CTkEntry(self.recording_duration_config_frame, placeholder_text="2", height=40, font=("Arial", 18))
+        self.action_duration = 0
         
         self.action_type_label = CTk.CTkLabel(self.recording_scheme_config_frame, text="Hành động tiếp theo:", wraplength=180, font=("Arial", 18))
         self.action_type_combo_box = CTk.CTkComboBox(self.recording_scheme_config_frame, values=["Nghỉ", "Gợi ý", "Tay trái", "Tay phải", "Chân trái", "Chân phải"],font=("Arial", 18), width=100)
@@ -193,7 +197,7 @@ class App(CTk.CTk):
         self.run_config_label = CTk.CTkLabel(self.recording_scheme_config_frame, text="", font=("Arial", 18))
         self.repeated_runs_label = CTk.CTkLabel(self.recording_scheme_config_frame, text="Số lần lặp lại kịch bản:", wraplength=180, font=("Arial", 18))
         self.repeated_runs_entry = CTk.CTkEntry(self.recording_scheme_config_frame, placeholder_text="1", height=40, font=("Arial", 18))
-        
+        self.repeated_runs = 0
         #Recording stage:
         self.sampling_frequency = 128
         
@@ -240,6 +244,8 @@ class App(CTk.CTk):
                                     onvalue=1, offvalue=0,
                                     switch_width=48, switch_height=27)
         self.display_mode_switch.grid(row=0, column=0, sticky="n")
+        
+        self.get_latest_scheme_config()
         
     def toggle_eeg_connection(self):
         if self.eeg_connection_flag.get() == 0:
@@ -299,26 +305,27 @@ class App(CTk.CTk):
 
     def add_action(self):
         action_type = None
+        
         value = self.action_type_combo_box.get()
         match value:
             case "Nghỉ":
                 action_type = Action.R
-                duration = int(self.rest_duration_entry.get())
+                duration = self.rest_duration
             case "Gợi ý":
                 action_type = Action.C
-                duration = int(self.cue_duration_entry.get())
+                duration = self.cue_duration
             case "Tay trái":
                 action_type = Action.LH
-                duration = int(self.action_duration_entry.get())
+                duration = self.action_duration
             case "Tay phải":
                 action_type = Action.RH
-                duration = int(self.action_duration_entry.get())
+                duration = self.action_duration
             case "Chân trái":
                 action_type = Action.LF
-                duration = int(self.action_duration_entry.get())
+                duration = self.action_duration
             case "Chân phải":
                 action_type = Action.RF
-                duration = int(self.action_duration_entry.get())
+                duration = self.action_duration
         if (duration != ""):
             action = (action_type, duration)
             self.recording_scheme_per_run.append(action)
@@ -334,7 +341,21 @@ class App(CTk.CTk):
             pass
 
     def update_run_config(self): 
-        cur = ""
+        self.rest_duration = int(self.rest_duration_entry.get())
+        self.cue_duration = int(self.cue_duration_entry.get())
+        self.action_duration = int(self.action_duration_entry.get())
+        self.repeated_runs = int(self.repeated_runs_entry.get())
+        
+        self.update_setting_label()
+        self.update_last_scheme_config()
+    
+    def update_setting_label(self):
+        if (self.rest_duration_entry.get() == ""): self.rest_duration_entry.insert(0, str(self.rest_duration))
+        if (self.cue_duration_entry.get() == ""): self.cue_duration_entry.insert(0, str(self.cue_duration))
+        if (self.action_duration_entry.get() == ""): self.action_duration_entry.insert(0, str(self.action_duration))
+        if (self.repeated_runs_entry.get() == ""): self.repeated_runs_entry.insert(0, str(self.repeated_runs))
+        
+        cur_action_names = ""
         for action in self.recording_scheme_per_run:
             action_name = ""
             match action[0]:
@@ -350,29 +371,74 @@ class App(CTk.CTk):
                     action_name = "Chân trái"
                 case Action.RF:
                     action_name = "Chân phải"
-            cur = action_name if (cur == "") else (cur + ", " + action_name)
+            cur_action_names = action_name if (cur_action_names == "") else (cur_action_names + ", " + action_name)
         print(self.recording_scheme_per_run)
-        self.run_config_label.configure(text=cur, font=("Arial", 18))
-        # generate_label_file()
+        self.run_config_label.configure(text=cur_action_names, font=("Arial", 18))
+        
+    def update_last_scheme_config(self):
+        scheme = []
+        for action in self.recording_scheme_per_run:
+            scheme.append(action[0].value)
+        last_config = {
+            "rest_duration": self.rest_duration,
+            "cue_duration": self.cue_duration,
+            "action_duration": self.action_duration,
+            "scheme": scheme,
+            "repeated_runs": self.repeated_runs
+        }
+        with open(f"last_config.json", "w") as file:
+            json.dump(last_config, file)
+            
+    def get_latest_scheme_config(self):
+        if os.path.exists(f"last_config.json"):
+            with open(f"last_config.json", "r") as file:
+                last_config = json.load(file)
+                print(last_config)
+                self.rest_duration = last_config["rest_duration"]
+                self.cue_duration = last_config["cue_duration"]
+                self.action_duration = last_config["action_duration"]
+                self.repeated_runs = last_config["repeated_runs"]
+                
+                cur_action_names = ""
+                for action_index in last_config["scheme"]:
+                    duration = 0
+                    match action_index:
+                        case 1: duration = self.rest_duration
+                        case 2: duration = self.cue_duration
+                        case _: duration = self.action_duration
+                    self.recording_scheme_per_run.append((Action(action_index), duration))
+                
+            self.update_setting_label()
+            print(self.recording_scheme_per_run)
+        else:
+            last_config = {
+                "rest_duration": self.rest_duration,
+                "cue_duration": self.cue_duration,
+                "action_duration": self.action_duration,
+                "scheme": [],
+                "repeated_runs": self.repeated_runs
+            }
+            with open(f"last_config.json", "w") as file:
+                json.dump(last_config, file)
             
     def generate_setting_file(self):
         setting = {
             "labels":{
-                "Rest": int(self.rest_duration_entry.get()),
-                "Cue":  int(self.cue_duration_entry.get()),
-                "Action.RH": int(self.action_duration_entry.get()),
-                "Action.LH": int(self.action_duration_entry.get()),
-                "Action.RF": int(self.action_duration_entry.get()),
-                "Action.LF": int(self.action_duration_entry.get()),
+                "Rest": self.rest_duration,
+                "Cue":  self.cue_duration,
+                "Action.RH": self.action_duration,
+                "Action.LH": self.action_duration,
+                "Action.RF": self.action_duration,
+                "Action.LF": self.action_duration,
             },
-            "num_of_runs": int(self.repeated_runs_entry.get())           
+            "num_of_runs": self.repeated_runs           
         }
         with open("data" + f"/{self.get_file_path('json')}", "w") as file:
             json.dump(setting, file)
                         
     def generate_label_file(self): #Call when recording is finished
         with open("data" + f"/{self.get_file_path('txt')}", "w") as file:
-            for i in range(0, int(self.repeated_runs_entry.get())):
+            for i in range(0, self.repeated_runs):
                 for action in self.recording_scheme_per_run:
                     file.write(f"{action[0].value}")
                         
@@ -382,7 +448,7 @@ class App(CTk.CTk):
         df.to_csv("data" + f"/{self.get_file_path('csv')}", index = False)
             
     def pull_eeg_data(self):
-        for i in range(0, int(self.repeated_runs_entry.get())):
+        for i in range(0, self.repeated_runs):
             global recording_in_progress
             cur_action_index = 0
             sample_count = 0
@@ -477,7 +543,7 @@ class App(CTk.CTk):
             if (len(self.recording_scheme_per_run) == 0):
                 self.show_error_message(self, error_message="Hãy nhập kịch bản thu")
             else:    
-                self.cue_window = CueWindow(window, self.recording_scheme_per_run, int(self.repeated_runs_entry.get()))
+                self.cue_window = CueWindow(window, self.recording_scheme_per_run, self.repeated_runs)
                 
                 self.cue_window.root = window
                 if self.cue_window.timerFlag == False:
