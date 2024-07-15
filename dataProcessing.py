6f68dea7bd89db6074991f09fa8ed1859f87d0cd
Nguyễn Minh Kiên <21020638@vnu.edu.vn>
2024-07-15 10:21:50 +0700

    Merge and Slice data

--- dataProcessing.py
+++ dataProcessing.py
@@ -23,37 +23,65 @@ def read_raw_data(dir):
     arr = np.delete(arr, 0, axis=0).transpose()
     return arr
 
+def process_VKIST_data(setup, X_train, y_train):
+    X_return = []
+    cur_sample, f = 0, 128
+    for i in range(0, 22):
+        X_return.append([])
+    for label in y_train[:]:
+        dur = setup.get('duration').get(action_list[label - 1])
+        match label:
+            case 1:
+                cur_sample += (dur * f)
+                y_train.remove(label)
+            case 2:
+                cur_sample += int((dur - 0.5) * f)
+                y_train.remove(label)
+            case default:
+                begin = cur_sample
+                end = cur_sample + int((dur + 0.5) * f)
+                for i in range(0, len(X_train)):
+                    X_return[i] = [*X_return[i], *X_train[i][begin : end]]
+                    cur_sample = end
+    X_return = np.array(X_return)
+    return X_return, y_train
+
 def load_VKIST_data(data_path, all_trials = True):
-    setup, X_train, y_train = None, None, None
+    setup, calibrated, no_runs, X_train, y_train = None, None, None, None, None
 
     for folder in os.listdir(data_source):
         date = os.path.join(data_source, folder)
         for scenario in os.listdir(date):
+            if scenario != session_scenario:
+                continue
             session = os.path.join(date, scenario)
             subject = os.path.join(session, subject_name)
             for file in os.listdir(subject):
                 if file.endswith(".csv"):
+                    # print(file)
                     if "Artifact" in file and all_trials == False:
-                        pass
+                        continue
+                    data = read_raw_data(os.path.join(subject, file))
+                    if X_train is not None:
+                        X_train = np.concatenate((X_train, data), axis=1)
                     else:
-                        data = read_raw_data(os.path.join(subject, file))
-                        if X_train is not None:
-                            X_train += data
-                        else:
-                            X_train = data
+                        X_train = data
                 elif file.endswith(".json") and setup == None:
                     setup = json.load(open(os.path.join(subject, file)))
+                    calibrated = setup.get('calibrated')
+                    no_runs = setup.get('num_of_runs')
                 elif file.endswith(".txt"):
+                    if "Artifact" in file and all_trials == False:
+                        continue   
                     labels = list(map(int, open(os.path.join(subject, file)).read().splitlines()))
                     if y_train is not None:
-                        y_train += labels
+                        y_train = y_train + labels
                     else:
                         y_train = labels
     
-    calibrated = setup.get('calibrated')
-    no_runs = setup.get('num_of_runs')
+    X_train, y_train = process_VKIST_data(setup, X_train, y_train)
 
-    return setup, calibrated, no_runs, X_train, labels
+    return setup, calibrated, no_runs, X_train, y_train
     
 
 setup, calibrated, no_runs, X_train, y_train = load_VKIST_data(data_source)
@@ -61,7 +89,8 @@ print(setup)
 print(calibrated)
 print(no_runs)
 print(X_train.shape)
-print(y_train)
+print(len(y_train))
+# print(y_train)
 
 # print(subject_name)
 
